7ebebb2fe6ee59806528dfd906d9bad4968a9268
takuto <takuto.yoshikai@gmail.com>
2020-03-21 19:06:33 +0900

    add shorten-url test

--- server_test.go
+++ server_test.go
@@ -3,14 +3,15 @@ package main
 import (
 	"bytes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"io/ioutil"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
 
-func SendJSON(r *gin.Engine, bodyString string) (int, string) {
-	req := httptest.NewRequest("POST", "/create", bytes.NewBuffer([]byte(bodyString)))
+func SendJSON(r *gin.Engine, path string, bodyString string) (int, string) {
+	req := httptest.NewRequest("POST", path, bytes.NewBuffer([]byte(bodyString)))
 	req.Header.Add("Content-Type", "application/json")
 	rec := httptest.NewRecorder()
 	r.ServeHTTP(rec, req)
@@ -19,16 +20,37 @@ func SendJSON(r *gin.Engine, bodyString string) (int, string) {
 	return rec.Code, string(body)
 }
 
+func GetRequest(r *gin.Engine, path string) (int, string) {
+	req := httptest.NewRequest("GET", path, nil)
+	rec := httptest.NewRecorder()
+	r.ServeHTTP(rec, req)
+	resp := rec.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	return rec.Code, string(body)
+}
+
 func TestServer(t *testing.T) {
-	InitDB()
+	err := godotenv.Load("env.test")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	InitDBForTest()
 	r := InitServer()
-	statusCode, responseText := SendJSON(r, `{"url":"https://yoshikai.net"}`)
+	statusCode, responseText := SendJSON(r, "/create", `{"url":"https://yoshikai.net"}`)
 	if statusCode != 200 {
 		t.Fatal("status code is not 200")
 		return
 	}
 	if !strings.HasPrefix(responseText, "http://localhost:8080/") {
-		t.Fatal("it is not a url")
+		t.Fatal("it is not a url" + responseText)
 		return
 	}
+
+	statusCode, _ = GetRequest(r, responseText)
+	if statusCode != 302 {
+		t.Fatal("didn't redirect")
+		return
+	}
+
 }
