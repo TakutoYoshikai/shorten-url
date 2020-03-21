ec60a77d150620ff58399a4d5dee7be377eaf0b7
takuto <takuto.yoshikai@gmail.com>
2020-03-21 18:02:57 +0900

    separate routing and func

--- server.go
+++ server.go
@@ -25,45 +25,53 @@ func CreateURLString(id string) string {
 	return protocol + "://" + host + "/" + id
 }
 
+func RedirectByIdRequest(c *gin.Context) {
+	id := c.Param("id")
+	url := GetURL(id)
+	if url == nil {
+		c.String(404, "")
+		return
+	}
+	c.Redirect(302, url.DstUrl)
+}
+
+func CreateURLRequest(c *gin.Context) {
+	var body URLRequestBody
+	c.BindJSON(&body)
+	url, errs := CreateURL(nil, body.Url)
+	if url == nil || (errs != nil && len(errs) > 0) {
+		c.String(400, "")
+		return
+	}
+	c.String(200, CreateURLString(url.SrcId))
+}
+
+func CreateUserRequest(c *gin.Context) {
+	var body CreateUserRequestBody
+	c.BindJSON(&body)
+	user, errs := CreateUser(body.Email, body.Password)
+	if user == nil || errs != nil && len(errs) > 0 {
+		c.String(400, "")
+		return
+	}
+	c.String(201, "")
+}
+
+func LoginRequest(c *gin.Context) {
+	var body LoginRequestBody
+	c.BindJSON(&body)
+	user := Login(body.Email, body.Password)
+	if user == nil {
+		c.String(401, "")
+		return
+	}
+}
+
 func InitServer() {
 	r := gin.Default()
-	r.GET("/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		url := GetURL(id)
-		if url == nil {
-			c.String(404, "")
-			return
-		}
-		c.Redirect(302, url.DstUrl)
-	})
-	r.POST("/create", func(c *gin.Context) {
-		var body URLRequestBody
-		c.BindJSON(&body)
-		url, errs := CreateURL(nil, body.Url)
-		if url == nil || (errs != nil && len(errs) > 0) {
-			c.String(400, "")
-			return
-		}
-		c.String(200, CreateURLString(url.SrcId))
-	})
-	r.POST("/user", func(c *gin.Context) {
-		var body CreateUserRequestBody
-		c.BindJSON(&body)
-		user, errs := CreateUser(body.Email, body.Password)
-		if user == nil || errs != nil && len(errs) > 0 {
-			c.String(400, "")
-			return
-		}
-		c.String(201, "")
-	})
-	r.GET("/login", func(c *gin.Context) {
-		var body LoginRequestBody
-		c.BindJSON(&body)
-		user := Login(body.Email, body.Password)
-		if user == nil {
-			c.String(401, "")
-			return
-		}
-	})
+	r.GET("/:id", RedirectByIdRequest)
+	r.POST("/create", CreateURLRequest)
+	r.POST("/user", CreateUserRequest)
+	r.GET("/login", LoginRequest)
 	r.Run()
 }
